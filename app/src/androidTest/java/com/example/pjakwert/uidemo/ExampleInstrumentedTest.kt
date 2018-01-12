d55a3486067b95f592937d3199edd3948f2f5e0d
Prem Jakwert <pjakwert@gmail.com>
2018-01-12 14:46:19 +0100

    test cleanup

--- app/src/androidTest/java/com/example/pjakwert/uidemo/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/pjakwert/uidemo/ExampleInstrumentedTest.kt
@@ -1,23 +1,19 @@
 package com.example.pjakwert.uidemo
 
-import android.app.PendingIntent.getActivity
 import android.support.test.InstrumentationRegistry
-import android.support.test.runner.AndroidJUnit4
-import android.support.test.espresso.Espresso.*
-import android.support.test.espresso.action.ViewActions.*
+import android.support.test.espresso.Espresso.onView
+import android.support.test.espresso.action.ViewActions.click
 import android.support.test.espresso.assertion.ViewAssertions.matches
 import android.support.test.espresso.matcher.ViewMatchers.*
 import android.support.test.filters.LargeTest
 import android.support.test.rule.ActivityTestRule
+import android.support.test.runner.AndroidJUnit4
 import android.test.ActivityInstrumentationTestCase2
 import org.hamcrest.Matchers
-
-import org.junit.Test
-import org.junit.runner.RunWith
-
-import org.junit.Assert.*
 import org.junit.Before
 import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -38,7 +34,6 @@ class ExampleInstrumentedTest : ActivityInstrumentationTestCase2<MainActivity>(M
     @Before
     public override fun setUp() {
         super.setUp()
-                //getActivity()
     }
 
 
@@ -51,7 +46,6 @@ class ExampleInstrumentedTest : ActivityInstrumentationTestCase2<MainActivity>(M
 
     @Test
     public fun navigationViewIsVisibleAfterClick() {
-        //mActivityRule.launchActivity(null)
         onView(withClassName( Matchers.equalToIgnoringCase("AppCompatImageButton"))).perform(click())
         onView(withId(R.id.design_navigation_view)).check(matches(isDisplayed()))
     }
